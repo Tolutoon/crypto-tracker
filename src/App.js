f24818e46041a0cd5054f0da40a2b4c4c414396a
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-06-21 14:18:38 +0100

    Apply Button on Hover Fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   const [coins, setCoins] = useState([]);
 
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=e';
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en';
 
   useEffect(()=>{
     axios.get(url).then((response) => {
